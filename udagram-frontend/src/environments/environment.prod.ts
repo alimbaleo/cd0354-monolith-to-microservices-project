dade58ed905e6b2484f79c78e6fbb2ec30acd5c7
Chukwuemeka <alimbaleoo@hotmail.com>
2022-09-26 22:31:07 +0100

    some bug fixes

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a07e4d5cef682445f8971bd288429234-1136796355.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
